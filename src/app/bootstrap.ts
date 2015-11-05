ad4f4ce03eb9b3d86dba681e7c8aa0ed5f57fe00
Mike Ryan <mike.ryan52@gmail.com>
2015-11-04 21:34:18 -0600

    Modified to work with ng-forward

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -3,9 +3,8 @@
 /*
  * Providers provided by Angular
  */
-import {bootstrap, FORM_PROVIDERS, ELEMENT_PROBE_PROVIDERS} from 'angular2/angular2';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {HTTP_PROVIDERS} from 'angular2/http';
+import 'angular';
+import {bootstrap} from 'ng-forward/cjs';
 
 /*
  * App Component
@@ -17,10 +16,4 @@ import {App} from './app';
  * Bootstrap our Angular app with a top level component `App` and inject
  * our Services and Providers into Angular's dependency injection
  */
-bootstrap(App, [
-  // These are dependencies of our App
-  FORM_PROVIDERS,
-  ROUTER_PROVIDERS,
-  HTTP_PROVIDERS,
-  ELEMENT_PROBE_PROVIDERS
-]);
+bootstrap(App, []);
