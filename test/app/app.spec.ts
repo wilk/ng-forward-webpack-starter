5bf05d81436786ac7ccbaddcea27d60232347739
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-01-09 17:44:31 +0100

    improved test: now they work fine

--- test/app/app.spec.ts
+++ test/app/app.spec.ts
@@ -1,9 +1,20 @@
 /// <reference path="../../src/typings/_custom.d.ts" />
 
+/**
+ * Module dependencies.
+ */
+import chai = require('chai');
+
+/**
+ * Globals
+ */
+
+var expect = chai.expect;
+
 describe('App', () => {
 
   it('should also be able to test', () => {
-    expect(true).toBe(true);
+    expect(true).to.equals(true);
   });
 
 });
