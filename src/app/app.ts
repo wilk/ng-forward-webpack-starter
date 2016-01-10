d930b454e73a1c1d46f03d45291187c4a7084eee
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2016-01-10 22:43:32 +0100

    improved example with a todo list

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,27 +15,37 @@ import {Component} from 'ng-forward/cjs';
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   template: `
   <header>
-    <h1>Hello {{ app.title }}</h1>
+    <h1>{{app.title}}</h1>
   </header>
 
   <main>
-    Your Content Here
+    Todo List
+
     <div>
-
-      <input type="text" ng-model="app.title" autofocus>
+      <input type="text" ng-keyup="app.addElement($event)" autofocus>
     </div>
-
-    <pre>this.title = {{ app.title | json }}</pre>
-    <pre>this.data = {{ app.data | json }}</pre>
-
+    <ul>
+      <li ng-repeat="element in app.list">{{element}}</li>
+    </ul>
   </main>
   `
 })
 export class App {
   // These are member type
   title: string;
-  data: Array<any> = []; // default data
+  list: string[];
+
   constructor() {
-    this.title = 'ng-forward';
+    this.title = 'ng-forward with Angular 1.x';
+    this.list = ['Study', 'Work', 'Party'];
+  }
+
+  addElement($event): void {
+    $event.preventDefault();
+
+    if ($event.keyCode === 13) {
+      this.list.push($event.target.value);
+      $event.target.value = '';
+    }
   }
 }
